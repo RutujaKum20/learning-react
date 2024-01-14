1c640ee11ca280fcf44830e9922efb83e861ee6a
Rutuja Kumbharkar <rutujakumbharkar20@gmail.com>
2024-01-14 16:01:10 +0530

    jsx and comonent composition

--- App.js
+++ App.js
@@ -1,27 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// ReactElement using createElement
-// ReactElement => object => HTMLELement(render)
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "React js Element using createElement !!!!!"
+//jsx React functional componemt
+// react functional component is a normal js fun which return a jsx code
+const HeadingComponent = () => (
+  <h1 className="heading">React functional component1</h1>
 );
 
-/*in this way it is very difficult to create complex html structure the react code will be not proper readable
- to overcome this react JSX comes into picture */
-
-//jsx
-const jsxHeading = <h1 className="jsxHeading">React using JSX</h1>;
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
 
-// root.render(heading);
-root.render(jsxHeading);
+//component composition  - > to render a component into another component
+const HeadingComponent1 = () => (
+  <div id="container">
+    <Title />
+    <Title></Title>
+    {Title}
+    <h1 className="heading">React functional component2</h1>
+  </div>
+);
+root.render(<HeadingComponent1 />);
 
-// jsx(transpiled before it reached to js) - parcel - Babel
-// jsx => balel transplied it to React.createElement => ReactElement -js object => HTMLElement(render)
-// jsx example
-// const jsxfooter = <h1 id="heading"> Hello React using jsx !!!!!!</h1>;
-// console.log(jsxfooter);
+// if i have to render title in the heading component
+const Title = () => (
+  <div id="container">
+    <h1 className="title">React functional component for composition </h1>
+  </div>
+);
+
+//javaScript inside jsx()
+const number = 1000;
+const JsInsideJsx = () => (
+  <div className="jsjsx">
+    <h2>{100 + 200}</h2>
+    <h1>{number}</h1>
+    {number}
+  </div>
+);
+
+//react element inside component
+const title = <h1>React element</h1>;
+const Header = () => (
+  <div id="container">
+    <h1>react jsx componrnt </h1>
+  </div>
+);
